561b7f516cf3267b6cd3c8282a93e02d9c9aa511
coderstephen <me@stephencoakley.com>
2015-05-15 02:12:40 -0500

    Only one other member in a DM channel

--- DirectMessageChannel.php
+++ DirectMessageChannel.php
@@ -1,6 +1,16 @@
 <?php
 namespace Slackyboy\Slack;
 
+/**
+ * Contains information about a direct message channel.
+ */
 class DirectMessageChannel extends Channel
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function getMembers()
+    {
+        yield $this->client->getUserById($this->data['user']);
+    }
 }
