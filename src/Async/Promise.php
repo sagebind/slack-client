aee90bc11b03d504c88bdeed7895dfbf3cd536e8
coderstephen <me@stephencoakley.com>
2015-06-01 17:35:33 -0500

    Added alien ooze that merges Guzzle and React loops into a single being

--- src/Async/Promise.php
+++ src/Async/Promise.php
@@ -1,16 +1,37 @@
 <?php
 namespace Slack\Async;
 
-use GuzzleHttp\Promise\Promise as GuzzlePromise;
+use GuzzleHttp\Promise as GuzzlePromise;
 use GuzzleHttp\Promise\PromiseInterface as GuzzlePromiseInterface;
+use React\EventLoop\LoopInterface;
 use React\Promise\PromiseInterface as ReactPromiseInterface;
 
 /**
  * A Frankenstein promise class that allows the Slack client to utilize both
  * ReactPHP and Guzzle asynchronous functions.
  */
-class Promise extends GuzzlePromise implements GuzzlePromiseInterface, ReactPromiseInterface
+class Promise extends GuzzlePromise\Promise implements GuzzlePromiseInterface, ReactPromiseInterface
 {
+    /**
+     * Adds a Guzzle task queue to a React event loop.
+     *
+     * @param LoopInterface $loop The event loop to add to.
+     */
+    public static function runQueue(LoopInterface $loop)
+    {
+        // Add the Guzzle queue to the React event loop via a periodic timer,
+        // which will be invoked when there are no more nextTick() callbacks.
+        // The timer self-cancels when the Guzzle queue reports that it is empty.
+        $queueTimer = $loop->addPeriodicTimer(0, function () use (&$queueTimer) {
+            $queue = GuzzlePromise\queue();
+            $queue->run();
+
+            if ($queue->isEmpty()) {
+                $queueTimer->cancel();
+            }
+        });
+    }
+
     /**
      * Creates a resolved promise with a given value.
      *
